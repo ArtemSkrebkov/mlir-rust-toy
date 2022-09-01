92b16d063bc804d45ae18106ede3bd5a0d9a316a
askrebko <skrebkov.art@gmail.com>
2022-09-01 16:03:23 +0300

    Add dummy Context, StandardDialect, ToyDialect

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,112 @@
+use std::ffi::CStr;
+
+use mlir_sys::{
+    mlirContextCreate, mlirContextGetOrLoadDialect, mlirDialectHandleGetNamespace,
+    mlirGetDialectHandle__std__,
+};
+use mlir_sys::{MlirContext, MlirDialectHandle};
+
+pub trait Dialect {
+    fn get_name(&self) -> String;
+}
+
+pub struct Context {
+    context: MlirContext,
+    dialects: Vec<Box<dyn Dialect>>,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        unsafe {
+            let context = mlirContextCreate();
+            Self {
+                context,
+                dialects: Vec::new(),
+            }
+        }
+    }
+
+    pub fn load_dialect(&mut self, dialect: Box<dyn Dialect>) {
+        self.dialects.push(dialect);
+        println!(
+            "Dialect {} loaded",
+            self.dialects.last().unwrap().get_name()
+        );
+    }
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+pub struct ToyDialect {
+    // context: &'a Context,
+// name
+}
+
+impl ToyDialect {
+    pub fn new(_context: &Context) -> Self {
+        Self {}
+    }
+}
+
+impl Dialect for ToyDialect {
+    fn get_name(&self) -> String {
+        String::from("toy")
+    }
+}
+
+pub struct StandardDialect {
+    std_handle: MlirDialectHandle,
+}
+
+impl StandardDialect {
+    pub fn new(_context: &Context) -> Self {
+        unsafe {
+            let std_handle = mlirGetDialectHandle__std__();
+            let std = mlirContextGetOrLoadDialect(
+                _context.context,
+                mlirDialectHandleGetNamespace(std_handle),
+            );
+            Self { std_handle }
+        }
+    }
+}
+
+impl Dialect for StandardDialect {
+    fn get_name(&self) -> String {
+        unsafe {
+            let namespace = mlirDialectHandleGetNamespace(self.std_handle);
+            let c_str: &CStr = unsafe { CStr::from_ptr(namespace.data) };
+            let str_slice: &str = c_str.to_str().unwrap();
+            let str_buf: String = str_slice.to_owned();
+            str_buf
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn create_context() {
+        let _context = Context::default();
+    }
+
+    #[test]
+    fn create_dialect() {
+        let context = Context::default();
+        let _dialect = ToyDialect::new(&context);
+    }
+
+    #[test]
+    fn load_dialect() {
+        let mut context = Context::default();
+        let dialect = ToyDialect::new(&context);
+        context.load_dialect(Box::new(dialect));
+
+        let std_dialect = StandardDialect::new(&context);
+        context.load_dialect(Box::new(std_dialect));
     }
 }
