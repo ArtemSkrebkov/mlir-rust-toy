b52846812965112a1dde7e54232234d8083252a4
askrebko <skrebkov.art@gmail.com>
2022-10-04 18:06:18 +0300

    Add dub transpose removal and infra for it

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod location;
 pub mod misc;
 pub mod op_builder;
 pub mod operation;
+pub mod pass_manager;
 pub mod toy;
 
 #[cfg(test)]
@@ -14,6 +15,7 @@ mod tests {
 
     use crate::context::Context;
     use crate::dialect::StandardDialect;
+    use crate::pass_manager::PassManager;
     use crate::toy::mlir_gen::MLIRGen;
     use crate::toy::parser;
     use crate::toy::toy_dialect::ToyDialect;
@@ -83,7 +85,6 @@ mod tests {
         prec.insert('/', 40);
 
         let context = Rc::new(Context::default());
-        // Need to make it mutable
         let dialect = ToyDialect::new(&context);
         context.load_dialect(Box::new(dialect));
         let module = parser::Parser::new(content, &mut prec)
@@ -91,6 +92,40 @@ mod tests {
             .unwrap();
         let module = MLIRGen::new(Rc::clone(&context)).mlir_gen(module);
         module.dump();
+        assert!(!module.block.operations.is_empty());
+    }
+
+    #[test]
+    fn generate_mlir_for_transpose_transpose_opt() {
+        let filename = "testdata/transpose_transpose_opt.toy".to_string();
+        if filename.is_empty() {
+            panic!("Cannot find file to read");
+        }
+        let content = std::fs::read_to_string(filename).unwrap();
+        let mut prec = HashMap::with_capacity(6);
+
+        prec.insert('=', 2);
+        prec.insert('<', 10);
+        prec.insert('+', 20);
+        prec.insert('-', 20);
+        prec.insert('*', 40);
+        prec.insert('/', 40);
+
+        let context = Rc::new(Context::default());
+        let dialect = ToyDialect::new(&context);
+        context.load_dialect(Box::new(dialect));
+        let module = parser::Parser::new(content, &mut prec)
+            .parse_module()
+            .unwrap();
+        let module = MLIRGen::new(Rc::clone(&context)).mlir_gen(module);
+
+        let pass_manager = PassManager::new(Rc::clone(&context));
+        let pass = PassManager::create_canonicalizer_pass();
+        pass_manager.add_nested_pass(pass, "builtin.func");
+        pass_manager.run(&module);
+
+        module.dump();
+        println!("");
         assert!(!module.block.operations.is_empty());
     }
 }
---
+++ src/pass_manager.rs
@@ -0,0 +1,48 @@
+use std::{ffi::CString, rc::Rc};
+
+use mlir_sys::{
+    mlirCreateTransformsCanonicalizer, mlirOpPassManagerAddOwnedPass, mlirPassManagerCreate,
+    mlirPassManagerGetNestedUnder, mlirPassManagerRun, mlirStringRefCreateFromCString, MlirPass,
+    MlirPassManager,
+};
+
+use crate::context::Context;
+
+pub struct PassManager {
+    instance: MlirPassManager,
+}
+
+impl PassManager {
+    pub fn new(context: Rc<Context>) -> Self {
+        let instance = unsafe { mlirPassManagerCreate(context.instance) };
+
+        Self { instance }
+    }
+
+    pub fn create_canonicalizer_pass() -> Pass {
+        let mlir_pass = unsafe { mlirCreateTransformsCanonicalizer() };
+        Pass {
+            instance: mlir_pass,
+        }
+    }
+
+    pub fn add_nested_pass(&self, pass: Pass, op_name: &str) {
+        unsafe {
+            let op_name = CString::new(op_name).unwrap();
+            let mlir_op_manager = mlirPassManagerGetNestedUnder(
+                self.instance,
+                mlirStringRefCreateFromCString(op_name.as_ptr()),
+            );
+            mlirOpPassManagerAddOwnedPass(mlir_op_manager, pass.instance);
+        }
+    }
+
+    pub fn run(&self, module: &crate::operation::ModuleOp) {
+        // check returned value
+        unsafe { mlirPassManagerRun(self.instance, module.instance) };
+    }
+}
+
+pub struct Pass {
+    instance: MlirPass,
+}
